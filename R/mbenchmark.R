2a67ef81c085ab480e49071ca0518af2bac47782
mikejiang <wjiang2@fhcrc.org>
2018-06-12 14:16:33 -0700

    add verbose

--- R/mbenchmark.R
+++ R/mbenchmark.R
@@ -16,11 +16,12 @@ mbenchmark.list <- function(x, access_pattern = c("region_selection", "random_sl
 #' @param shape vectors of colsize-to-rowsize ratio, which defines the shape of the selected region
 #' @param nstride the number of regions to test. with the dimension size evenly increase up to the ubound
 #' @param ubound the upper bound of size of region to select. It is the percentage of the maximum rows
+#' @importFrom plyr ldply
 #' @examples
 #' mat <- matrix(seq_len(2e6), nrow = 1e3, ncol =2e3)
 #' res <- mbenchmark_region_selection(mat, ubound = 1)
 #' autoplot(res)
-mbenchmark_region_selection <- function(x, shape = c(0.5, 1, 2), nstride = 3, ubound = 0.1,  num_threads = 1, times = 5L, clear_page_cache = FALSE){
+mbenchmark_region_selection <- function(x, shape = c(0.5, 1, 2), nstride = 5, ubound = 0.1,  num_threads = 1, times = 5L, clear_page_cache = FALSE, verbose = TRUE){
   if(!is.list(x))
     x <- list(a = x)
 
@@ -31,29 +32,33 @@ mbenchmark_region_selection <- function(x, shape = c(0.5, 1, 2), nstride = 3, ub
   row_start <- col_start <- 1
   row_stride <- floor(nrow * ubound/nstride)
   res <- ldply(shape, function(ratio){
+    message("region shape (nrow / ncol):", shape)
     col_stride <- floor(row_stride * ratio)
     res <- ldply(seq_len(nstride), function(i)
     {
       # browser()
       row_size <- row_stride * i
-      row_end <- row_start + row_size
+      row_end <- row_start + row_size - 1
       ridx <- row_start:row_end
       col_size <- col_stride * i
-      col_end <- col_start + col_size
+      col_end <- col_start + col_size - 1
       if(col_end>ncol)
       {
         warning("col idx out of bound!", col_end)
         return(NULL)
       }
       cidx <- col_start:col_end
-      # message(row_end, ":", col_end)
+      if(verbose)
+        message("row = ", row_start, ":", row_end, " col = ", col_start, ":", col_end)
       res <- ldply(seq_len(times), function(j)
       {
         if(clear_page_cache)
         {
           stop("clear_page_cache not supported yet!")
         }
-        res <- ldply(x, function(mat){
+        res <- ldply(names(x), function(dsname){
+          # message(dsname)
+          mat <- x[[dsname]]
           system.time(v <- mat[ridx, cidx])[["elapsed"]]
         })
         res[["timeid"]] <- j
