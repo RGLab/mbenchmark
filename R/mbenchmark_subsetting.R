b6434fb369e7a75a589296d495995036a9e9074e
mikejiang <wjiang2@fhcrc.org>
2018-06-20 11:20:36 -0700

    add cached result mechanism

--- R/mbenchmark_subsetting.R
+++ R/mbenchmark_subsetting.R
@@ -10,7 +10,16 @@
 #' res <- mbenchmark_subsetting(mat.list, times = 2, nsubset = 3)
 #' autoplot(res)
 #' }
-mbenchmark_subsetting <- function(x, type = c("random_slicing", "region_selection"), shape = c(0.5, 1, 2), nsubset = 5, ubound = 0.1,  num_threads = 1, times = 5L, clear_page_cache = FALSE, trace_mem = FALSE, verbose = TRUE){
+mbenchmark_subsetting <- function(x, type = c("random_slicing", "region_selection")
+                                  , shape = c(0.5, 1, 2)
+                                  , nsubset = 5
+                                  , ubound = 0.1
+                                  , num_threads = 1
+                                  , times = 5L
+                                  , clear_page_cache = FALSE
+                                  , trace_mem = FALSE
+                                  , cache.file = tempfile()
+                                  , verbose = FALSE){
   if(!is.list(x))
     x <- list(a = x)
 
@@ -23,15 +32,15 @@ mbenchmark_subsetting <- function(x, type = c("random_slicing", "region_selectio
   ncol <- dims[2]
   row_start <- col_start <- 1
   row_stride <- floor(nrow * ubound/nsubset)
-  res <- .ldply(type, function(thistype){
+  lapply(type, function(thistype){
     message(thistype)
     #interate over different subset shapes
-    res <- .ldply(shape, function(ratio){
-      message("subset shape (nrow / ncol):", ratio)
+    lapply(shape, function(ratio){
+      # message("subset shape (nrow / ncol):", ratio)
       col_stride <- floor(row_stride / ratio)
 
       #iterate over subsets of different sizes
-      res <- .ldply(seq_len(nsubset), function(i)
+      lapply(seq_len(nsubset), function(i)
       {
         # browser()
         row_size <- row_stride * i
@@ -65,65 +74,92 @@ mbenchmark_subsetting <- function(x, type = c("random_slicing", "region_selectio
         }
 
 
-        if(thistype == "region_selection")
-          if(verbose)
-            message("row = ", row_start, ":", row_end, " col = ", col_start, ":", col_end)
+        # if(thistype == "region_selection")
+          # if(verbose)
+            # message("row = ", row_start, ":", row_end, " col = ", col_start, ":", col_end)
 
         #iterate multiple times
-        res <- .ldply(seq_len(times), function(j)
+        lapply(seq_len(times), function(j)
         {
-          if(clear_page_cache)
+          if(file.exists(cache.file))
           {
-            stop("clear_page_cache not supported yet!")
+            cache.res <- fread(cache.file)
+            cached.sub <- cache.res[timeid == j & nrow == row_size & `nrow/ncol` == ratio & task == thistype,]
+            is.cached <- nrow(cached.sub) >0
+          }else
+          {
+            is.cached <- FALSE
+            cache.res <- NULL
           }
 
-          records <- new.env(parent = emptyenv())
 
-          res <- .ldply(names(x), function(dsname){
-            #track mem
-            if(trace_mem && num_threads == 1 && j == 1)
+          if(!is.cached)
+          {
+            if(clear_page_cache)
             {
-              gc()#return mem to os
-              gc()
-              mem <- as.integer(system(cmd, intern = TRUE))
+              stop("clear_page_cache not supported yet!")
             }
 
+            records <- new.env(parent = emptyenv())
 
-            t <- system.time(v <- as.matrix(x[[dsname]][ridx, cidx]))[["elapsed"]]
+            res <- .ldply(names(x), function(dsname){
+              #track mem
+              if(trace_mem && num_threads == 1 && j == 1)
+              {
+                gc()#return mem to os
+                gc()
+                mem <- as.integer(system(cmd, intern = TRUE))
+              }
 
-            #track mem change
-            if(trace_mem && num_threads == 1 && j == 1)
+
+              t <- system.time(v <- as.matrix(x[[dsname]][ridx, cidx]))[["elapsed"]]
+
+              #track mem change
+              if(trace_mem && num_threads == 1 && j == 1)
+              {
+                mem <- as.integer(system(cmd, intern = TRUE)) - mem
+                gc()#return mem to os
+                gc()#somehow need it twice to be sussessful
+              }else
+                mem <- 0
+
+
+              #validity check on the results
+              if(j==1)#only check at first timeiteration
+                if(is.null(records[["first"]]))
+                  records[["first"]] <- v#assign the value from the first ds
+                else
+                  if(!isTRUE(all.equal(records[["first"]], v, check.attributes = FALSE)))
+                    stop("results from ", dsname, " are not the same as the other dataset!")
+
+                dt <- data.table(time = t, mem_change = mem, dataset = dsname, timeid = j, nrow = row_size, `nrow/ncol` = ratio, task = thistype)
+                if(verbose)
+                  print(dt)
+                dt
+
+            })#iter over datasets
+
+            #cache it to disk to avoid recompute next time when resume
+            if(is.null(cache.res))
+              cache.res <- res
+            else
+              cache.res <- rbind(cache.res, res)
+            fwrite(cache.res, file = cache.file)
+          }else
+          {
+            if(verbose)
             {
-              mem <- as.integer(system(cmd, intern = TRUE)) - mem
-              gc()#return mem to os
-              gc()#somehow need it twice to be sussessful
-            }else
-              mem <- 0
+              message("skip cached items: ")
+              print(cached.sub[1, list(timeid, nrow, `nrow/ncol`, task)])
+            }
 
+          }
 
-            #validity check on the results
-            if(j==1)#only check at first timeiteration
-              if(is.null(records[["first"]]))
-                records[["first"]] <- v#assign the value from the first ds
-              else
-                if(!isTRUE(all.equal(records[["first"]], v, check.attributes = FALSE)))
-                  stop("results from ", dsname, " are not the same as the other dataset!")
-
-              data.table(time = t, mem_change = mem, dataset = dsname)
-          })
-
-
-          set(res, j = "timeid", value = j)
-
-        })
-        # set(res, j = "size", value = paste0(row_size, "x", col_size))
-        set(res, j = "nrow", value = row_size)
-        res
-      })
-      set(res, j = "nrow/ncol", value = ratio)
+        })#iter over times
+      })#iter over shapes
     })
-    res[, task := thistype]
   })
+  res <- fread(cache.file)
   attr(res, "class") <- c("mbenchmark_subsetting", "mbenchmark",attr(res, "class"))
   res
 }
--- README.md
+++ README.md
@@ -1,29 +1,27 @@
----
-title: "mbenchmark: benchmarking the common matrix operations"
-output: github_document
----
+mbenchmark: benchmarking the common matrix operations
+================
 
-
-
-## Introduction
+Introduction
+------------
 
 This is a generic framework for benchmarking the common matrix operations among different matrix file formats.
 
-* subsetting
-    * `region_selection`: continuous block selection
-    * `random_slicing`: non-continuous slab selection
-* traversing
-    * `rowSums`
-    * `colSums`
+-   subsetting
+    -   `region_selection`: continuous block selection
+    -   `random_slicing`: non-continuous slab selection
+-   traversing
+    -   `rowSums`
+    -   `colSums`
 
-## Takes a list of different `matrix` containers/objects as input
+Takes a list of different `matrix` containers/objects as input
+--------------------------------------------------------------
+
 The only requirements for the `matrix` object are:
 
-* support `[` indexing method for subsetting
-* `dim` accessors to retrieve dimension information
+-   support `[` indexing method for subsetting
+-   `dim` accessors to retrieve dimension information
 
-
-```r
+``` r
 library(HDF5Array)#must load it first to avoid namespace conflicting
 library(mbenchmark)
 mat <- matrix(seq_len(2e6), nrow = 1e3, ncol =2e3)
@@ -51,142 +49,123 @@ library(ff)
 ff.file <- tempfile()
 fm <- ff(mat, vmode="double", dim=dims, filename = ff.file)
 fm <- DelayedArray(fm)
-```
-## Compare disk usage
 
-```r
-mat.list <- list(bigmemory = bm, ff = fm, h5 = hm)
+library(matter)
+mm <- matter_mat(mat)
+mm <- DelayedArray(mm)
+```
+
+Compare disk usage
+------------------
+
+``` r
+mat.list <- list(bigmemory = bm, ff = fm, h5 = hm, matter = mm)
 
 utils:::format.object_size(file.size(bm.file), units = "Mb")
 ```
 
-```
-## [1] "7.6 Mb"
-```
+    ## [1] "7.6 Mb"
 
-```r
+``` r
 utils:::format.object_size(file.size(h5.file), units = "Mb")
 ```
 
-```
-## [1] "2.8 Mb"
-```
+    ## [1] "2.8 Mb"
 
-```r
+``` r
 utils:::format.object_size(file.size(ff.file), units = "Mb")
 ```
 
-```
-## [1] "15.3 Mb"
-```
-## Compare memory usage
+    ## [1] "15.3 Mb"
 
-```r
+``` r
+utils:::format.object_size(file.size(matter::paths(mm@seed)), units = "Mb")
+```
+
+    ## [1] "7.6 Mb"
+
+Compare memory usage
+--------------------
+
+``` r
 library(pryr)
 lapply(mat.list, object_size)
 ```
 
-```
-## $bigmemory
-## 2.55 kB
-## 
-## $ff
-## 2.01 kB
-## 
-## $h5
-## 1.97 kB
-```
+    ## $bigmemory
+    ## 2.55 kB
+    ## 
+    ## $ff
+    ## 2.01 kB
+    ## 
+    ## $h5
+    ## 1.97 kB
+    ## 
+    ## $matter
+    ## 9.18 kB
 
+Run `subsetting` benchmark
+--------------------------
 
-
-## Run `subsetting` benchmark
-
-```r
+``` r
 #ubound specify the upper bound of the size of the subset. It is the value of the maximum percentage of original matrix
 res <- mbenchmark(mat.list, type = "subsetting", times = 3, ubound = 0.9, trace_mem = TRUE, verbose = FALSE) 
 ```
 
-```
-## random_slicing
-```
+    ## random_slicing
 
-```
-## subset shape (nrow / ncol):0.5
-```
-
-```
-## subset shape (nrow / ncol):1
-```
-
-```
-## subset shape (nrow / ncol):2
-```
-
-```
-## region_selection
-```
-
-```
-## subset shape (nrow / ncol):0.5
-```
-
-```
-## subset shape (nrow / ncol):1
-```
-
-```
-## subset shape (nrow / ncol):2
-```
-
+    ## region_selection
 
 Results are collected as a `data.table`, which is easy to query or facetting.
 
-```r
+``` r
 head(res)
 ```
 
+    ##     time mem_change   dataset timeid nrow nrow/ncol           task
+    ## 1: 0.025         24 bigmemory      1  180       0.5 random_slicing
+    ## 2: 0.011         28        ff      1  180       0.5 random_slicing
+    ## 3: 0.572          0        h5      1  180       0.5 random_slicing
+    ## 4: 0.054         20    matter      1  180       0.5 random_slicing
+    ## 5: 0.007          0 bigmemory      2  180       0.5 random_slicing
+    ## 6: 0.010          0        ff      2  180       0.5 random_slicing
+
+Resume the long-running task by `cached` results
+------------------------------------------------
+
+Some time things could go wrong before the entire benchmarking completes. To save the time, a cached file can be passed to the `mbenchmark` so that when this command is re-executed the sub-tasks previously saved in this file will be skipped.
+
+``` r
+cachefile <- tempfile()
+res <- mbenchmark(mat.list, type = "subsetting", cache.file = cachefile) 
 ```
-##     time mem_change   dataset timeid nrow nrow/ncol           task
-## 1: 0.015          0 bigmemory      1  180       0.5 random_slicing
-## 2: 0.009         -4        ff      1  180       0.5 random_slicing
-## 3: 0.236          0        h5      1  180       0.5 random_slicing
-## 4: 0.007          0 bigmemory      2  180       0.5 random_slicing
-## 5: 0.008          0        ff      2  180       0.5 random_slicing
-## 6: 0.146          0        h5      2  180       0.5 random_slicing
-```
 
-## Quick plot
+Quick plot
+----------
 
-
-```r
+``` r
 autoplot(res)
 ```
 
-```
-## Error in grid.Call(C_textBounds, as.graphicsAnnot(x$label), x$x, x$y, : X11 font -adobe-helvetica-%s-%s-*-*-%d-*-*-*-*-*-*-*, face 1 at size 9 could not be loaded
-```
+![](README_files/figure-markdown_github/unnamed-chunk-7-1.png)
 
-![plot of chunk unnamed-chunk-6](README_files/figure-markdown_github/unnamed-chunk-6-1.png)
-
-
-```r
+``` r
 plot_mem(res, units = "Kb")
 ```
 
-```
-## Error in grid.Call(C_textBounds, as.graphicsAnnot(x$label), x$x, x$y, : X11 font -adobe-helvetica-%s-%s-*-*-%d-*-*-*-*-*-*-*, face 1 at size 9 could not be loaded
-```
+![](README_files/figure-markdown_github/unnamed-chunk-8-1.png)
 
-![plot of chunk unnamed-chunk-7](README_files/figure-markdown_github//unnamed-chunk-7-1.png)
+Run `traversing` benchmark
+--------------------------
 
-## Run `traversing` benchmark
-
-```r
+``` r
 mat.list <- list(bigmemory = bm, ff = fm, h5 = hm)
 res <- mbenchmark(mat.list, type = "traversing")
 ```
 
-## Todo
-* `clear_page_cache`
-* `parallel` IO
-* More matrix operations
+Todo
+----
+
+-   `clear_page_cache`
+-   `parallel` IO
+-   More matrix operations
